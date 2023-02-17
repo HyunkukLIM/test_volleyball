4b27fd5d4ed4beb7186a8823005a9d0101b3d269
hyunkuk.lim <hyunkuk.lim@sports2i.com>
2023-02-17 15:24:16 +0900

    Startlist Controller에 insert Lineup 기능 추가

--- test_volleyball/src/main/java/com/sports2i/test_volleyball/controller/StartlistController.java
+++ test_volleyball/src/main/java/com/sports2i/test_volleyball/controller/StartlistController.java
@@ -1,5 +1,7 @@
 package com.sports2i.test_volleyball.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sports2i.test_volleyball.dto.ResponseDto;
 import com.sports2i.test_volleyball.dto.StartlistDto;
+import com.sports2i.test_volleyball.service.ComputingService;
 import com.sports2i.test_volleyball.service.StartlistService;
 
 @RestController
@@ -17,17 +20,20 @@ public class StartlistController {
 	@Autowired
 	private StartlistService startlistService;
 	
-	@GetMapping("/api/play/selectPlayerList")
+	@Autowired
+	private ComputingService computingService;
+	
+	@GetMapping("/api/startlist/selectPlayerList")
 	public ResponseDto<?> searchPlayerList () {
 //		System.out.println("여기까지 들어옴");
 		return new ResponseDto<>(HttpStatus.OK.value(), startlistService.searchPlayerList());
 	}
 	
 	@PostMapping("/api/startlist/insertLineup")
-	public ResponseDto<?> insertLineup (@RequestBody StartlistDto.Request request) {		
-		System.out.println(request);
+	public ResponseDto<?> insertLineup (@RequestBody List<StartlistDto.Request> requests) {		
+//		System.out.println(request);
 		
-//		startlistService.savePlayerInfo(request);
+		computingService.insertLineup(requests);
 		
 		return new ResponseDto<>(HttpStatus.OK.value(), 1);
 	}
