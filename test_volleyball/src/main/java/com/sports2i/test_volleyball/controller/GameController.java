4277ee34778e4d11f40005349e8916d4df8849f4
hyunkuk.lim <hyunkuk.lim@sports2i.com>
2023-02-21 10:22:14 +0900

    insertSet 함수에 @RequestBody 선언!

--- test_volleyball/src/main/java/com/sports2i/test_volleyball/controller/GameController.java
+++ test_volleyball/src/main/java/com/sports2i/test_volleyball/controller/GameController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sports2i.test_volleyball.dto.GameDto;
@@ -34,10 +35,12 @@ public class GameController {
 	}
 	
 	@PostMapping("/api/game/insertSet")
-	public ResponseDto<?> insertSet(GameDto.Request request) {
+	public ResponseDto<?> insertSet(@RequestBody GameDto.Request request) {
 		
 		gameService.insertSet(request);		
 		
 		return new ResponseDto<>(HttpStatus.OK.value(), 1);
 	}
+	
+	
 }
